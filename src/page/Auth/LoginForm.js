26550338d950334a2cfc8b43f5cc3b7414797c5a
gminnimk <tvt1245@naver.com>
2024-10-25 19:01:12 +0900

    fix : 백엔드 구조와 맞게 수정 및 프론트 연동 성공

--- src/page/Auth/LoginForm.js
+++ src/page/Auth/LoginForm.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import { login } from '../../api/auth';
+import React, {useState} from 'react';
+import {login} from '../../api/auth';
 import './LoginForm.css';
 
-const redirectToKakaoLogin = () => {
+const redirectToKakaoLogin = (e) => {
+  e.preventDefault(); // 기본 폼 제출 막기
   const KAKAO_CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID; // 환경 변수에서 클라이언트 ID 가져오기
   const KAKAO_REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI; // 환경 변수에서 리다이렉트 URI 가져오기
 
@@ -10,7 +11,8 @@ const redirectToKakaoLogin = () => {
   window.location.href = kakaoLoginUrl;
 };
 
-const redirectToGoogleLogin = () => {
+const redirectToGoogleLogin = (e) => {
+  e.preventDefault();
   const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
   const GOOGLE_REDIRECT_URI = process.env.REACT_APP_GOOGLE_REDIRECT_URI;
 
@@ -18,8 +20,7 @@ const redirectToGoogleLogin = () => {
   window.location.href = googleLoginUrl;
 };
 
-
-const LoginForm = ({ toggleMode, setMessage, setIsMessageVisible }) => {
+const LoginForm = ({toggleMode, setMessage, setIsMessageVisible}) => {
   const [formData, setFormData] = useState({
     username: '',
     password: ''
@@ -38,9 +39,9 @@ const LoginForm = ({ toggleMode, setMessage, setIsMessageVisible }) => {
     try {
       const response = await login(formData);
 
-      if (response.status === 200) {
+      if (response.data) {
         setMessage('로그인 성공!');
-        localStorage.setItem('access_token', response.data.token);  // 로그인 성공 시 JWT 토큰 저장
+        localStorage.setItem('access_token', response.data); // 로그인 성공 시 JWT 토큰 저장
       } else {
         setMessage('로그인 실패. 다시 시도해 주세요.');
       }
