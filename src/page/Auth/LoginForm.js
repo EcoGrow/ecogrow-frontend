c6d33e59c06cb576bc01252c77f2a491eb92952a
gminnimk <tvt1245@naver.com>
2024-10-27 17:02:49 +0900

    feat : 로그인 성공 시 MainPage 리디렉션 설정

--- src/page/Auth/LoginForm.js
+++ src/page/Auth/LoginForm.js
@@ -1,11 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 import {login} from '../../api/auth';
 import './LoginForm.css';
 
 const redirectToKakaoLogin = (e) => {
-  e.preventDefault(); // 기본 폼 제출 막기
-  const KAKAO_CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID; // 환경 변수에서 클라이언트 ID 가져오기
-  const KAKAO_REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI; // 환경 변수에서 리다이렉트 URI 가져오기
+  e.preventDefault();
+  const KAKAO_CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
+  const KAKAO_REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI;
 
   const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
   window.location.href = kakaoLoginUrl;
@@ -23,13 +24,15 @@ const redirectToGoogleLogin = (e) => {
 const LoginForm = ({toggleMode, setMessage, setIsMessageVisible}) => {
   const [formData, setFormData] = useState({
     username: '',
-    password: ''
+    password: '',
   });
+  const navigate = useNavigate();
+
 
   const handleInputChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
@@ -41,19 +44,20 @@ const LoginForm = ({toggleMode, setMessage, setIsMessageVisible}) => {
 
       if (response.data) {
         setMessage('로그인 성공!');
-        localStorage.setItem('access_token', response.data); // 로그인 성공 시 JWT 토큰 저장
+        localStorage.setItem('token', response.data); // JWT 토큰 저장
+        setIsMessageVisible(true);
+        setTimeout(() => {
+          setIsMessageVisible(false);
+          navigate('/'); // 로그인 성공 시 MainPage로 리디렉션
+        }, 1000);
       } else {
         setMessage('로그인 실패. 다시 시도해 주세요.');
+        setIsMessageVisible(true);
       }
     } catch (error) {
       setMessage('로그인 실패. 다시 시도해 주세요.');
+      setIsMessageVisible(true);
     }
-
-    setIsMessageVisible(true);
-    setTimeout(() => {
-      setIsMessageVisible(false);
-      setMessage('');
-    }, 3000);
   };
 
   return (
@@ -65,6 +69,7 @@ const LoginForm = ({toggleMode, setMessage, setIsMessageVisible}) => {
             className="input"
             name="username"
             onChange={handleInputChange}
+            required
         />
         <input
             type="password"
@@ -72,6 +77,7 @@ const LoginForm = ({toggleMode, setMessage, setIsMessageVisible}) => {
             className="input"
             name="password"
             onChange={handleInputChange}
+            required
         />
         <div className="social-login">
           <button className="social-btn google-btn"
@@ -91,7 +97,9 @@ const LoginForm = ({toggleMode, setMessage, setIsMessageVisible}) => {
             Sign in with Kakao
           </button>
         </div>
-        <button className="btn" type="submit">Sign In</button>
+        <button className="btn" type="submit">
+          Sign In
+        </button>
         <p>
           Don't have an account?{' '}
           <span className="toggle-link" onClick={toggleMode}>
