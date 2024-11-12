289e6718b363cbe647d7206e397814c236ef864b
gminnimk <tvt1245@naver.com>
2024-11-12 19:59:39 +0900

    feat : 쓰레기 유형 및 재활용 여부 그래프에 'kg' 레이블 추가하여 그래프 위에 마우스 올릴 시 시각적으로 단위 확인

--- src/page/WasteRecord.js
+++ src/page/WasteRecord.js
@@ -28,6 +28,7 @@ const WasteRecord = () => {
       'Metal (Recyclable)', 'Organic (Non-Recyclable)',
       'General Waste (Non-Recyclable)'],
     datasets: [{
+      label: 'kg',
       data: [],
       backgroundColor: ['rgba(255, 99, 132, 0.6)', 'rgba(54, 162, 235, 0.6)',
         'rgba(255, 206, 86, 0.6)', 'rgba(75, 192, 192, 0.6)',
